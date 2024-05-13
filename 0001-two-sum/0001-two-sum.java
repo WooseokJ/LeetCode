be600221bae3d38369c7f7bdeb04a1acc64b70d6
Wooseok <wooseokbird@gmail.com>
2024-05-13 21:45:11 +0900

    Time: 38 ms (45.65%), Space: 44.6 MB (69.42%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,14 +1,10 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // 2중 for문 
         int n = nums.length;
-        for(int i =0; i < n; i++) {
-            for(int j = i+1; j < n ; j++) {
-                if(target == nums[i] + nums[j])
+        for(int i = 0; i < n; i++) 
+            for(int j = i+1; j < n ;j++) 
+                if(target == nums[i] + nums[j]) 
                     return new int[]{i,j};
-            }
-        }
-        
         return null;
     }
 }
