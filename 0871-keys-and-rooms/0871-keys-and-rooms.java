c31b160c5c0c121418c96000c1126bfc8427ab8a
Wooseok <wooseokbird@gmail.com>
2024-05-16 15:56:14 +0900

    Time: 4 ms (12.21%), Space: 44.3 MB (39.28%) - LeetHub

--- 0871-keys-and-rooms/0871-keys-and-rooms.java
+++ 0871-keys-and-rooms/0871-keys-and-rooms.java
@@ -1,32 +1,33 @@
 import java.util.*;
-import java.util.stream.*;
+
 class Solution {
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
-        boolean[] visited = new boolean[rooms.size()];
+        Map<Integer, Boolean> visited = new HashMap<>();
+        for(int i = 0; i < rooms.size(); i++) {
+            visited.put(i,false);
+        }
         bfs(0, visited, rooms);
 
-
-        // 
-        for(boolean select: visited) {
-            if(!select) {
-                return false;
-            }
-        }        
+        for(Map.Entry<Integer, Boolean> entry : visited.entrySet()) {
+            Boolean select = entry.getValue();
+            if(!select) return false;
+        }
         return true;
 
 
     }
-    public static void bfs(int start_node, boolean[] visited, List<List<Integer>> rooms) {
+    public static void bfs(int start_node, Map<Integer, Boolean> visited, List<List<Integer>> rooms) {
+
         Deque<Integer> q = new ArrayDeque<>();
-        visited[start_node] = true;
+        visited.put(start_node, true);
         q.offer(start_node);
         while(!q.isEmpty()) {
             int cur_node = q.poll();
             List<Integer> keySet = rooms.get(cur_node);
             for(Integer next_node: keySet) {
-                if(!visited[next_node]) {
+                if(!visited.get(next_node)) {
                     q.offer(next_node);
-                    visited[next_node] = true;
+                    visited.put(next_node,true);
                 }
             }
 
