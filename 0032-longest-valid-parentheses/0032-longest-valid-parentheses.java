ff7c34ea46ac66628c15574c536745cf5ded1273
Wooseok <wooseokbird@gmail.com>
2024-05-18 21:50:57 +0900

    Time: 4 ms (32.8%), Space: 43 MB (27.3%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
@@ -4,23 +4,32 @@ class Solution {
         int ans = 0;
         int n = str.length();
         
-        Deque<Integer> stack = new ArrayDeque<>();
-        stack.push(-1);
+        Stack<Pair> stack = new Stack<>();
+        stack.add(new Pair(-1, ')'));
 
-        for(int i =0 ; i <n; i++) {
-            Character keyword = str.charAt(i);
+        for(int idx =0 ; idx <n; idx++) {
+            Character keyword = str.charAt(idx);
             if(keyword == '(') {
-                stack.push(i);
-            } else {
+                stack.add(new Pair(idx, keyword));
+            } else { // ')'
                 stack.pop();
                 if(stack.isEmpty()) {
-                    stack.push(i);
+                    stack.add(new Pair(idx, ')'));
                 } else {
-                    ans = Math.max(ans, i - stack.peek());
+                    int len = idx - stack.peek().index;
+                    ans = Math.max(ans,len);
                 }
-
             }
         }
+
         return ans;
+    }
+    public static class Pair {
+        int index;
+        Character keyword;
+        public Pair(int index, Character keyword) {
+            this.index = index;
+            this.keyword = keyword;
+        }
     }
 }
