b33fa6de46d5769f0177350d8a58a1543886a65d
Wooseok <wooseokbird@gmail.com>
2024-05-19 00:35:12 +0900

    Time: 1 ms (71.12%), Space: 46.1 MB (52.18%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -2,31 +2,39 @@ class Solution {
     public int trap(int[] height) {
         int ans = 0;
         int n = height.length;
-        int l = 0;
-        int r = n - 1;
-        int lmax = height[0];
-        int rmax = height[n-1];
+        Pair l = new Pair(0, height[0]);
+        Pair r = new Pair(n-1, height[n-1]);
 
-        while(l < r) { // 
-            if(lmax <= rmax) { // l이 기준이됨.
-                l++;
-                int cur_h = height[l];
-                if(cur_h < lmax) { // l기준보다 내려가있으면
-                    ans += lmax - cur_h;
+        while(l.pos < r.pos) { // 
+            if(l.max <= r.max) { // l이 기준이됨.
+                l.pos++;
+                int cur_h = height[l.pos];
+                if(cur_h < l.max) { // l기준보다 내려가있으면
+                    ans += l.max - cur_h;
                 } else { // l기준보다 올라가있어
-                    lmax = cur_h; // 높이를 바꿔
+                    l.max = cur_h; // 높이를 바꿔
                 }
             } else { // r 이 기준이됨.
-                r--;
-                int cur_h = height[r];
-                if(cur_h < rmax) { // r기준보다 내려가있으면
-                    ans += rmax - cur_h;
+                r.pos--;
+                int cur_h = height[r.pos];
+                if(cur_h < r.max) { // r기준보다 내려가있으면
+                    ans += r.max - cur_h;
                 } else { // r기준보다 높으면
-                    rmax = cur_h;
+                    r.max = cur_h;
                 }
             }
         }
         return ans;
         
     }
+
+    public static class Pair {
+        int pos;
+        int max;
+        public Pair(int pos, int max) {
+            this.pos = pos;
+            this.max = max;
+        }
+    }
+
 }
