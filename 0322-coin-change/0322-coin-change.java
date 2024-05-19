ca756de66e7b1276f5ea3b72f0f71abc2cd56061
Wooseok <wooseokbird@gmail.com>
2024-05-19 15:28:38 +0900

    Time: 23 ms (37.41%), Space: 44.6 MB (38.14%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,20 +1,23 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        if(amount == 0) {return 0;}
-        boolean[] visited = new boolean[amount+1];    
+        // 예외 반환
+        if(amount == 0) {return 0;} 
+
+        // 
+        boolean[] visited = new boolean[amount+1];    // 0 ~ 11으로 노드에대한 방문여부판단.
         
         Deque<Pair> q = new ArrayDeque<>();
-        q.offer(new Pair(amount, 0));
+        q.offer(new Pair(amount, 1)); // 시작을 amount 와 detph 1로 시작.
 
         while(!q.isEmpty()) {
-            Pair cur = q.poll();
+            Pair cur = q.poll(); 
             for(int i = 0; i < coins.length; i++) {
                 int nextAmount = cur.amount - coins[i];
                 if(nextAmount == 0) {
-                    return cur.cnt+1;
+                    return cur.depth ;
                 } else if(nextAmount > 0){
                     if(!visited[nextAmount]) {
-                        q.offer(new Pair(nextAmount, cur.cnt+1));
+                        q.offer(new Pair(nextAmount, cur.depth+1));
                         visited[nextAmount] = true;
                     }
                 }
@@ -28,10 +31,10 @@ class Solution {
 
     public static class Pair {
         int amount;
-        int cnt;
-        public Pair(int amount, int cnt) {
+        int depth;
+        public Pair(int amount, int depth) {
             this.amount = amount;
-            this.cnt = cnt;
+            this.depth = depth;
         }
     }
 
