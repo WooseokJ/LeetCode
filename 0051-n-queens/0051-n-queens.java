7c98229b607f5086fa9491d4ce22ee7e595593bb
Wooseok <wooseokbird@gmail.com>
2024-06-02 16:46:52 +0900

    Time: 131 ms (5.29%), Space: 44.5 MB (81.01%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -2,29 +2,32 @@ class Solution {
     public List<List<String>> solveNQueens(int n) {
         List<List<String>> ans = new ArrayList<>();
 
-        char[][] grid = new char[n][n];
-        int rowLen = grid.length;
-        int colLen = grid[0].length;
+		// n * n board생성
 
+        char[][] board = new char[n][n];
+        int rowLen = board.length;
+        int colLen = board[0].length;
+        
         for(int r = 0; r < rowLen; r++) {
             for(int c =0 ; c < colLen; c++) {
-                grid[r][c] = '.';
+                board[r][c] = '.'; //모두다 . 으로 채움.
             }
         }
 
-        int curCnt = 0;
-        backtrack(0,curCnt, n, grid, ans);
+        int curCnt = 0;  // 현재 퀸의 개수 
+        backtrack(0,curCnt, n, board, ans);
         
         return ans;
 
     }
-    public static void backtrack(int r, int curCnt, int n, char[][] grid, List<List<String>> ans) {
-        int rowLen = grid.length;
-        int colLen = grid[0].length;
+    public static void backtrack(int r, int curCnt, int n, char[][] board, List<List<String>> ans) {
+        int rowLen = board.length;
+        int colLen = board[0].length;
 
+		// 현재 퀸의개수와 n이 같으면 체스판 추가.
         if(r == n) {
             List<String> temp = new ArrayList<>();
-            for(char[] row: grid) {
+            for(char[] row: board) {
                 temp.add(new String(row));
             }
 
@@ -32,28 +35,32 @@ class Solution {
             ans.add(temp);
             return ;
         }
+        // row는 정해졋고 col을 순회
         for(int c = 0; c < colLen; c++) {
-            if(isValid(r, c, grid)) {
-                grid[r][c] = 'Q';
-                backtrack(r+1, curCnt+1, n, grid, ans);
-                grid[r][c] = '.';
+            if(isValid(r, c, board)) {
+                board[r][c] = 'Q';
+                backtrack(r+1, curCnt+1, n, board, ans);
+                board[r][c] = '.';// 재귀 끝나면
             }
         }
     }
-    public static boolean isValid(int r, int c, char[][] grid) {
-        int rowLen = grid.length;
-        int colLen = grid[0].length;
-        // 위쪽 
+    public static boolean isValid(int r, int c, char[][] board) {
+        int rowLen = board.length;
+        int colLen = board[0].length;
+		// 열 확인 ⬆️
         for(int curRow = 0; curRow < r; curRow++) {
-            if(grid[curRow][c] == 'Q') return false;
+            if(board[curRow][c] == 'Q') return false;
         }
-        // 왼쪽위대각선
+		// 왼쪽 대각선 확인. ↖️
+		// row-1은 위로한칸, col-1은 왼쪽한칸
         for(int curRow = (r -1), curCol = (c - 1) ; 0 <= curRow && 0 <= curCol ; curRow--, curCol--) {
-            if(grid[curRow][curCol] == 'Q') return false;
+            if(board[curRow][curCol] == 'Q') return false;
         }
-        // 오른쪽위대각선
+     		
+		// 오른쪽 대각선 확인. ↗️
+		// row -1 은 위로한칸, col+1은 오른쪽 한칸
         for(int curRow = r - 1,  curCol = c + 1; 0 <= curRow && curCol < colLen; curRow--, curCol++) {
-            if(grid[curRow][curCol] == 'Q') return false;
+            if(board[curRow][curCol] == 'Q') return false;
         }
         return true;
     }
