c0ce245de3a1407279526ef72eb479f8c3c799a5
Wooseok <wooseokbird@gmail.com>
2024-06-03 17:35:31 +0900

    Time: 14 ms (65.15%), Space: 46.9 MB (91.14%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.java
+++ 0744-network-delay-time/0744-network-delay-time.java
@@ -1,47 +1,42 @@
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
-        // 방향 그래프 만들기.
         Map<Integer, List<int[]>> graph = new HashMap<>();
         for(int[] timeInfo: times) {
             int node = timeInfo[0];
-            int connectNode = timeInfo[1];
-            int time= timeInfo[2];
-
-            graph.putIfAbsent(node,new ArrayList<>());
-            graph.get(node).add(new int[]{node, connectNode, time});
+            int nextNode = timeInfo[1];
+            int weight = timeInfo[2];
+            graph.putIfAbsent(node, new ArrayList<>());
+            graph.get(node).add(new int[] {nextNode, weight});
         }
 
-        int[] visited= new int[n+1];
+        int[] visited = new int[n+1];
         int INF = Integer.MAX_VALUE;
         Arrays.fill(visited, INF);
 
-        // 큐에 {노드, 거리 } 배열 들어감.
-        Queue<int[]> pq = new PriorityQueue<>( (e1,e2) -> e1[1] - e2[1] );
+        Queue<int[]> pq = new PriorityQueue<>((e1, e2) -> e1[1] - e2[1]);
         pq.offer(new int[]{k,0});
         visited[k] = 0;
 
-        //  k부터 모든노드 최단거리 구하기 (다익스트라 수행)
-        int ans = 0;
-        int visitedCnt = 1;
+        int ans =0;
+        int visitedCnt =1;
         while(!pq.isEmpty()) {
             int[] cur = pq.poll();
-            int u = cur[0];
-            int time = cur[1];
-            if(visited[u] < time) continue;
-            ans = time;
+            int curNode = cur[0];
+            int curWeight = cur[1];
+            if(visited[curNode] < curWeight ) continue;
+            ans = curWeight;
 
             // 연결된 간선 탐색
-            if(!graph.containsKey(u)) continue;
-            for(int[] edge: graph.get(u)) {
-                int v = edge[1];
-                int w = edge[2];
-                if(time + w >= visited[v]) continue;
-                if(visited[v] == INF) {visitedCnt++;} 
-                visited[v] = time + w;
-                pq.offer(new int[] {v, time + w});
+            if(!graph.containsKey(curNode)) continue;
+            for(int[] edge: graph.get(curNode)) {
+                int nextNode = edge[0];
+                int nextWeight = edge[1];
+                if(visited[nextNode] <= curWeight + nextWeight) continue;
+                if(visited[nextNode] == INF) visitedCnt++;
+                visited[nextNode] = curWeight + nextWeight;
+                pq.offer(new int[]{nextNode, curWeight + nextWeight});
             }
         }
-
         if(visitedCnt == n) return ans;
         else return -1;
     }
