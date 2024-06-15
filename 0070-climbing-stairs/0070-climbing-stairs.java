5541257743778dd7530c1b1a91f113e6f9a50f34
Wooseok <wooseokbird@gmail.com>
2024-06-15 22:21:12 +0900

    Time: 0 ms (100%), Space: 40.1 MB (54.14%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,14 +1,37 @@
+import java.util.*;
 class Solution {
     // base case 
     // top down VS bottom up
 
     public int climbStairs(int n) {
         // return topdown(n);
-        return downup(n);
+        // return downup(n);
 
-
-      
+        return downup2(n);
     }
+    public static int topdown2(int n) {
+        Map<Integer, Integer> memo = new HashMap<>();
+        return dp2(memo, n);
+    }
+    public static int dp2(Map<Integer, Integer> memo, int n) {
+        if(n == 0 || n == 1) return 1;
+        if(!memo.containsKey(n)) {
+            int sum = dp2(memo, n-1) + dp2(memo, n-2);
+            memo.put(n, sum);
+        } 
+        return memo.get(n);
+    }
+    public static int downup2(int n) {
+        Map<Integer, Integer> memo = new HashMap<>();
+        memo.put(0,1);
+        memo.put(1,1);
+        for(int i = 2; i <= n; i++) {
+            int sum = memo.get(i-1) + memo.get(i-2);
+            memo.put(i,sum);
+        }
+        return memo.get(n);
+    }
+    
     public static int downup(int n ) {
         // bottom up dp(0) + dp(1) = dp(2)
         int[] memo = new int[n+1];
