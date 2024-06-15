ab4d0b7e9e9ba8dcd46be50280b692886bb1e62e
Wooseok <wooseokbird@gmail.com>
2024-06-15 21:48:36 +0900

    Time: 0 ms (100%), Space: 40.4 MB (29.02%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -2,19 +2,19 @@ class Solution {
     public int climbStairs(int n) {
         // top down   dp(2) = dp(0) + dp(1)
         // 계산된거 저장하는 memo선언.
-        // int[] memo = new int[n+1];
-        // Arrays.fill(memo,-1); // -1,-1,-1 .. 
-        // return dp(memo, n);
+        int[] memo = new int[n+1];
+        Arrays.fill(memo,-1); // -1,-1,-1 .. 
+        return dp(memo, n);
 
 
         // bottom up dp(0) + dp(1) = dp(2)
-        int[] memo = new int[n+1];
-        // base case 미리 
-        memo[0] = 1;
-        memo[1] = 1;
-        for(int i = 2; i <= n ; i++)
-            memo[i] = memo[i-1] + memo[i-2];
-        return memo[n];
+        // int[] memo = new int[n+1];
+        // // base case 미리 
+        // memo[0] = 1;
+        // memo[1] = 1;
+        // for(int i = 2; i <= n ; i++)
+        //     memo[i] = memo[i-1] + memo[i-2];
+        // return memo[n];
     }
 
 
