0559d82c6f90918f37a855eb41f80fff2eb40e01
Wooseok <wooseokbird@gmail.com>
2024-06-15 22:29:50 +0900

    Time: 1 ms (40.34%), Space: 43.4 MB (17.73%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.java
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.java
@@ -4,6 +4,37 @@ class Solution {
     public int minCostClimbingStairs(int[] cost) {
         // return topdown(cost);
         return bottomup(cost);
+        // Map 사용
+
+        // return topdown2(cost);
+        // return downup2(cost); // 4초 
+    }
+    public static int downup2(int[] cost) {
+        int n = cost.length;
+        Map<Integer, Integer> memo = new HashMap<>();
+        memo.put(1,0);
+        memo.put(0,0);
+        for(int i =2; i <= n ; i++) {
+            int min = Math.min(memo.get(i-1) + cost[i-1], 
+                                memo.get(i-2) + cost[i-2]);
+            memo.put(i, min);
+        }
+        return memo.get(n);
+    }
+
+        
+    public static int topdown2(int[] cost) {
+        int n = cost.length;
+        Map<Integer, Integer> memo = new HashMap<>();
+        return dp2(memo,cost, n);
+    }
+    public static int dp2(Map<Integer, Integer> memo,int[] cost, int n) {
+        if(n == 0 || n == 1) return 0;
+        if(!memo.containsKey(n)) {
+            memo.put(n,Math.min(dp2(memo, cost, n-1) + cost[n-1], dp2(memo,cost, n-2) + cost[n-2]));
+        }
+        return memo.get(n);
+            
     }
 
     public static int bottomup(int[] cost) {
