47b3812ec6b5f799bac78b7ccaf88b1e34a04ddc
Wooseok <wooseokbird@gmail.com>
2024-06-18 14:44:44 +0900

    Time: 55 ms (28.84%), Space: 44.3 MB (39.83%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,13 +1,39 @@
 import java.util.*;
 
 class Solution {
+    static int ans = 0;
     public int lengthOfLIS(int[] nums) {
-        
-        return downup(nums);
+        // return downup(nums);
+        return lis(nums, nums.length);
     }
+
+  // lis() returns the length of the longest
+    // increasing subsequence in arr[] of size n
+    public static int lis(int arr[], int n) {
+        int lis[] = new int[n];
+        int i, j, max = 0;
+
+        // Initialize LIS values for all indexes
+        for (i = 0; i < n; i++)
+            lis[i] = 1;
+
+        // Compute optimized LIS values in
+        // bottom up manner
+        for (i = 1; i < n; i++)
+            for (j = 0; j < i; j++)
+                if (arr[i] > arr[j] && lis[i] < lis[j] + 1)
+                    lis[i] = lis[j] + 1;
+
+        // Pick maximum of all LIS values
+        for (i = 0; i < n; i++)
+            if (max < lis[i])
+                max = lis[i];
+
+        return max;
+    }
+
     // down up
     public static int downup(int[] nums) {
-        if(nums.length == 0) return 0;
         // idx, 길이
         Map<Integer, Integer> memo = new HashMap<>();
         memo.put(0,1); // 0번쨰 인덱스 의 길이 1 
