bb626e0ee466295a4fabd8ef1de6c515bc6721f0
Wooseok <wooseokbird@gmail.com>
2024-06-18 14:47:03 +0900

    Time: 53 ms (30.03%), Space: 43.9 MB (68.16%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -4,27 +4,22 @@ class Solution {
     static int ans = 0;
     public int lengthOfLIS(int[] nums) {
         // return downup(nums);
-        return lis(nums, nums.length);
+        return topdown(nums, nums.length);
     }
 
-  // lis() returns the length of the longest
-    // increasing subsequence in arr[] of size n
-    public static int lis(int arr[], int n) {
+    public static int topdown(int arr[], int n) {
         int lis[] = new int[n];
         int i, j, max = 0;
-
-        // Initialize LIS values for all indexes
+        
+        // 다 1 로 바꿔.
         for (i = 0; i < n; i++)
             lis[i] = 1;
-
-        // Compute optimized LIS values in
-        // bottom up manner
+        // 순회 
         for (i = 1; i < n; i++)
-            for (j = 0; j < i; j++)
-                if (arr[i] > arr[j] && lis[i] < lis[j] + 1)
+            for (j = 0; j < i; j++) // 0~j까지 순회
+                if (arr[i] > arr[j] && (lis[i] < lis[j] + 1)) // 기준 숫자가 더 크고 
                     lis[i] = lis[j] + 1;
 
-        // Pick maximum of all LIS values
         for (i = 0; i < n; i++)
             if (max < lis[i])
                 max = lis[i];
