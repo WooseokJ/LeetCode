62fb60904cfcc5e40f32780c6092c3e0f95b87a8
Wooseok <wooseokbird@gmail.com>
2024-06-18 13:50:46 +0900

    Time: 640 ms (5.02%), Space: 45.7 MB (20.18%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        return topdown(nums);
+    }
+    // top down
+    public static int topdown(int[] nums) {
+        // idx, 길이
+        Map<Integer, Integer> memo = new HashMap<>();
+        memo.put(0,1); // 0번쨰 인덱스 의 길이 1 
+        int ans = 1;
+        for(int i = 1; i < nums.length; i++) {
+            memo.put(i,1);
+            // j = 0~i까지 순회
+            for(int j = 0; j < i; j++) {
+                if(nums[j] < nums[i]) {
+                    int val = Math.max(memo.get(i),
+                                   memo.get(j) + 1);
+                    memo.put(i, val);
+                }
+        
+            }
+            ans = Math.max(memo.get(i), ans);
+        }
+        return ans;
+    }
+}
