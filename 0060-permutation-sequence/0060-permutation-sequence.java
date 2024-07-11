0444354adc5ffecb8cbac44676ebebd45334921b
Wooseok <wooseokbird@gmail.com>
2024-07-11 11:17:58 +0900

    Time: 865 ms (12.13%), Space: 231.8 MB (5.05%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.java
+++ 0060-permutation-sequence/0060-permutation-sequence.java
@@ -1,33 +1,46 @@
 import java.util.*;
-
 class Solution {
     public String getPermutation(int n, int k) {
-        List<List<Integer>> list = new ArrayList();
-        permutation(n, k, list, new ArrayList<>(), new boolean[n+1]);
-        String s = "";
-        List<Integer> target = list.get(k-1);
-        for(int i=0; i<n; i++) {
-            s = s + Integer.toString(target.get(i));
-        }
-        return s;
+        List<List<String>> ans = new ArrayList<>();
+        String[] nums = new String[n+1];
+        for(int i = 1; i <= n ; i++)
+            nums[i] = Integer.toString(i);
+
+        StringBuilder sb = new StringBuilder();
+        boolean[] visited = new boolean[n+1];
+        backtrack(new ArrayList<>(), nums,n, k, visited, ans);
+        
+        List<String> target = ans.get(k-1);
+        for(String str: target)
+            sb.append(str);
+
+        return sb.toString();
     }
 
-    private void permutation(int n, int k, List<List<Integer>> list, ArrayList<Integer> now, boolean[] visited) {
-        if(now.size() == n) {
-            list.add(new ArrayList<>(now));
-            return;
+    public static void backtrack(List<String> curr,String[] nums,int n, int k, boolean[] visited, List<List<String>> ans ) {
+    if(curr.size() == n) {
+        ans.add(new ArrayList<>(curr));
+        return;
+    }
+
+
+    // 이거 안하면 타임아웃(k번쨰 만큼 넣으면 뺴줘 )
+    if(ans.size() == k) {
+        return ;
+    }
+    
+
+    for(int i =1; i<= n; i++) {
+        if(!visited[i]) {
+            visited[i] = true;
+            curr.add(Integer.toString(i));
+            backtrack(curr, nums, n, k, visited, ans);
+            curr.remove(curr.size()-1);
+            visited[i] = false;
         }
-        if(list.size() == k) {
-            return;
-        }
-        for(int i=1; i<=n; i++) {
-            if(!visited[i]) {
-                now.add(i);
-                visited[i] = true;
-                permutation(n, k, list, now, visited);
-                now.remove(now.size() - 1);
-                visited[i] = false;
-            }
-        }
+    }
+        
+    
+        
     }
 }
