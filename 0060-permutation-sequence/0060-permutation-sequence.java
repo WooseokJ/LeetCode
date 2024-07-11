0005a4a3a784840a8eb7427b38452ee5171624db
Wooseok <wooseokbird@gmail.com>
2024-07-11 11:14:57 +0900

    Time: 557 ms (14.99%), Space: 114.3 MB (5.05%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.java
+++ 0060-permutation-sequence/0060-permutation-sequence.java
@@ -1,25 +1,33 @@
+import java.util.*;
+
 class Solution {
     public String getPermutation(int n, int k) {
-        List<Integer> nums = new ArrayList<>();
-        for(int i = 1; i <= n; i++)
-            nums.add(i);
-        
-        StringBuilder ans = new StringBuilder();
-        
-        while (n > 1 ) {
-            int page = factorial(n - 1);
-            int idx = (k - 1) / page;
-            k = (k-1) % page + 1;
-            ans.append(nums.get(idx));
-            nums.remove(idx);
-            n--;
+        List<List<Integer>> list = new ArrayList();
+        permutation(n, k, list, new ArrayList<>(), new boolean[n+1]);
+        String s = "";
+        List<Integer> target = list.get(k-1);
+        for(int i=0; i<n; i++) {
+            s = s + Integer.toString(target.get(i));
         }
-        ans.append(nums.get(0));
-        return ans.toString();
+        return s;
     }
 
-    public static int factorial(int n) {
-        if(n == 1 | n == 0) return 1;
-        return n * factorial(n-1);
+    private void permutation(int n, int k, List<List<Integer>> list, ArrayList<Integer> now, boolean[] visited) {
+        if(now.size() == n) {
+            list.add(new ArrayList<>(now));
+            return;
+        }
+        if(list.size() == k) {
+            return;
+        }
+        for(int i=1; i<=n; i++) {
+            if(!visited[i]) {
+                now.add(i);
+                visited[i] = true;
+                permutation(n, k, list, now, visited);
+                now.remove(now.size() - 1);
+                visited[i] = false;
+            }
+        }
     }
 }
