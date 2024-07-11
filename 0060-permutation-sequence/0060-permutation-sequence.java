a64387d2b3bba4e6d31d74284a1e8fe8d19b529b
Wooseok <wooseokbird@gmail.com>
2024-07-11 11:29:47 +0900

    Time: 838 ms (12.22%), Space: 231.5 MB (5.05%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.java
+++ 0060-permutation-sequence/0060-permutation-sequence.java
@@ -1,46 +1,40 @@
 import java.util.*;
+
 class Solution {
     public String getPermutation(int n, int k) {
         List<List<String>> ans = new ArrayList<>();
         String[] nums = new String[n+1];
-        for(int i = 1; i <= n ; i++)
+        for(int i =1; i <= n;i++)
             nums[i] = Integer.toString(i);
-
-        StringBuilder sb = new StringBuilder();
         boolean[] visited = new boolean[n+1];
-        backtrack(new ArrayList<>(), nums,n, k, visited, ans);
-        
+        backtrack(new ArrayList<>(), n, k, nums, visited, ans);
+
         List<String> target = ans.get(k-1);
-        for(String str: target)
+        
+        StringBuilder sb =  new StringBuilder();
+        for(String str: target) {
             sb.append(str);
-
-        return sb.toString();
-    }
-
-    public static void backtrack(List<String> curr,String[] nums,int n, int k, boolean[] visited, List<List<String>> ans ) {
-    if(curr.size() == n) {
-        ans.add(new ArrayList<>(curr));
-        return;
-    }
-
-
-    // 이거 안하면 타임아웃(k번쨰 만큼 넣으면 뺴줘 )
-    if(ans.size() == k) {
-        return ;
-    }
-    
-
-    for(int i =1; i<= n; i++) {
-        if(!visited[i]) {
-            visited[i] = true;
-            curr.add(Integer.toString(i));
-            backtrack(curr, nums, n, k, visited, ans);
-            curr.remove(curr.size()-1);
-            visited[i] = false;
         }
+        return sb.toString();
+
     }
-        
-    
-        
+    public static void backtrack(List<String> curr, int n , int k , String[] nums, boolean[] visited, List<List<String>> ans) {
+        if(curr.size() == n) {
+            ans.add(new ArrayList<>(curr));
+            return ;
+        }
+        if(ans.size() == k){
+            return;
+        }
+        for(int i =1; i <= n; i++) {
+            if(!visited[i]) {
+                visited[i] = true;
+                curr.add(Integer.toString(i));
+                backtrack(curr, n, k, nums, visited, ans);
+                curr.remove(curr.size()-1);
+                visited[i] = false;
+            }
+        }
+
     }
 }
