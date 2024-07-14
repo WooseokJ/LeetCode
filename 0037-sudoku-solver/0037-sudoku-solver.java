3d2a3674994c96c7fb3e1d84f9e7c4c5b6f83eee
Wooseok <wooseokbird@gmail.com>
2024-07-14 16:54:45 +0900

    Time: 13 ms (9.09%), Space: 41.2 MB (26.23%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -4,30 +4,48 @@ class Solution {
         backtrack(board, 0);
     }
     public static boolean backtrack(char[][] board, int n) {
-	    
-		   for(int r = 0; r < 9; r++) {
-			   for(int c = 0; c < 9 ; c++) {
-				   if(board[r][c] == '.') {
-					   for(char num = '1'; num <= '9'; num++) {
-						   if(isValid(r, c, num, board)) {
-							   board[r][c] = num;
-							   if(backtrack(board, n+1)) return true;
-							   board[r][c] = '.';
-						   }
-					   }
-					   
-					   return false;
-				   }
-			   }
-		   }
-		   
-		   return true;
+		    //step2. 현재, row, col 각각 구하기.
+        int row = n / 9; 
+        int col = n % 9;
+	
+				// step3. basecase설정
+        if(n == 81) return true; // 마지막이면 반환
+        // step4. 마침표로 분기처리
+        if(board[row][col] == '.') { 
+		        // 숫자 비어있으면 1~9 에대해 유효성검사
+            for(char num = '1'; num <= '9'; num++) {
+                if(isValid(row, col, num, board)) {
+                    board[row][col] = num;
+                    if(backtrack(board, n+1)) return true; // n+1 은 오른쪽으로 이동.
+                    board[row][col] = '.';
+                }
+            }
+            return false;
+        } else {
+		        // 숫자 안비어있으면 다음 보드 탐색.
+            return backtrack(board, n+1);
+        }
     }
-		public static boolean isValid(int r, int c, char num, char[][] board) {
-			for(int i = 0; i < 9; i++) {
-				if(board[i][c] == num || board[r][i] == num) return false;
-				if(board[(r/3) * 3 + i /3][(c/3) * 3 + i % 3] == num) return false;
-			}
-			return true;
-	  }
+    public static boolean isValid(int row, int col, int num ,char[][] board) {
+        for(int i =0 ;i < 9 ;i++) {
+            // 가로 ,세로 같은숫자있는지 판단  (이미 사용된적있는수이면 false반환)
+            if( (board[i][col] == num) || (board[row][i] == num) ) return false;
+
+
+            // 3*3안에서 이미 사용된적있는 수이면 false반환 (풀이 1) - 시간 세이브됨.(나의풀이)
+//            int in_row = 3 *(row / 3 ) + (i / 3);
+//            int in_col = 3 * (col /3 ) + (i % 3);
+//            if(board[in_row][in_col] == num) return false;
+
+            // 3*3안에서 이미 사용된적있는 수이면 false반환( 풀이 2) - 직관적이라 좋음.(해답코드), 대신 for문 또 돌아서 더오래걸림.
+            int in_row = 3 * (row / 3);
+            int in_col = 3 * (col / 3);
+            for(int dr = 0 ; dr <3; dr++)
+                for(int dc = 0; dc <3; dc++)
+                    if(board[in_row+dr][in_col+dc] == num) return false;
+				   
+        }
+        return true;
+        
+    }
 }
