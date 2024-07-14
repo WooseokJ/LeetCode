ec4fb5f57c8e649be1d016ab0de844d3750b17a4
Wooseok <wooseokbird@gmail.com>
2024-07-14 16:52:16 +0900

    Time: 10 ms (46.5%), Space: 40.6 MB (84.96%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,43 +1,33 @@
 class Solution {
     public void solveSudoku(char[][] board) {
+		    //step1. 재귀로 모든 board 순회( 시작은 0 )
         backtrack(board, 0);
     }
     public static boolean backtrack(char[][] board, int n) {
-        int row = n / 9;
-        int col = n % 9;
-
-        if(n == 81) return true; // 마지막이면 반환
-        if(board[row][col] == '.') {
-            for(char num = '1'; num <= '9'; num++) {
-                if(isValid(row, col, num, board)) {
-                    board[row][col] = num;
-                    if(backtrack(board, n+1)) return true;
-                    board[row][col] = '.';
-                }
-            }
-            return false;
-        } else {
-            return backtrack(board, n+1);
-        }
+	    
+		   for(int r = 0; r < 9; r++) {
+			   for(int c = 0; c < 9 ; c++) {
+				   if(board[r][c] == '.') {
+					   for(char num = '1'; num <= '9'; num++) {
+						   if(isValid(r, c, num, board)) {
+							   board[r][c] = num;
+							   if(backtrack(board, n+1)) return true;
+							   board[r][c] = '.';
+						   }
+					   }
+					   
+					   return false;
+				   }
+			   }
+		   }
+		   
+		   return true;
     }
-    public static boolean isValid(int row, int col, int num ,char[][] board) {
-        for(int i =0 ;i < 9 ;i++) {
-            // 가로 ,세로 같은숫자있는지 판단
-            if( (board[i][col] == num) || (board[row][i] == num) ) return false;
-            // 3 * 3 내부에서 판단.
-            
-            // int in_row = 3 *(row / 3 ) + (i / 3);
-            // int in_col = 3 * (col /3 ) + (i % 3);
-            // if(board[in_row][in_col] == num) return false;d
-            int in_row = 3 * (row / 3);
-            int in_col = 3 * (col / 3);
-            for(int dr = 0 ; dr < 3; dr++) {
-                for(int dc = 0; dc < 3; dc++) {
-                    if(board[in_row + dr][in_col+dc] == num) return false;
-                }
-            }
-        }
-        return true;
-        
-    }
+		public static boolean isValid(int r, int c, char num, char[][] board) {
+			for(int i = 0; i < 9; i++) {
+				if(board[i][c] == num || board[r][i] == num) return false;
+				if(board[(r/3) * 3 + i /3][(c/3) * 3 + i % 3] == num) return false;
+			}
+			return true;
+	  }
 }
