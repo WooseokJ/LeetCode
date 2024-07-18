32b73d7bbd31a022982a1ed537780d642a4665a8
Wooseok <wooseokbird@gmail.com>
2024-07-18 20:48:36 +0900

    Time: 58 ms (7.27%), Space: 46.3 MB (29.23%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -2,39 +2,43 @@ class Solution {
     public int trap(int[] height) {
         int ans = 0;
         int n = height.length;
+
         Pair l = new Pair(0, height[0]);
         Pair r = new Pair(n-1, height[n-1]);
+        System.out.println(l.max + " " + r.max);
 
-        while(l.pos < r.pos) { // 
-            if(l.max <= r.max) { // l이 기준이됨.
+        while(l.pos < r.pos) {
+            if(l.max < r.max) {
                 l.pos++;
-                int cur_h = height[l.pos];
-                if(cur_h < l.max) { // l기준보다 내려가있으면
-                    ans += l.max - cur_h;
-                } else { // l기준보다 올라가있어
-                    l.max = cur_h; // 높이를 바꿔
+                int curHeight = height[l.pos];
+                if(curHeight > l.max) {
+                    l.max = curHeight;
+                    
+                    System.out.println(l.pos + " l");
+                } else {
+                    ans += Math.abs(curHeight-l.max);
                 }
-            } else { // r 이 기준이됨.
+            } else {
                 r.pos--;
-                int cur_h = height[r.pos];
-                if(cur_h < r.max) { // r기준보다 내려가있으면
-                    ans += r.max - cur_h;
-                } else { // r기준보다 높으면
-                    r.max = cur_h;
+                int curHeight = height[r.pos];
+                if(curHeight > r.max) {
+                    r.max = curHeight;
+                    System.out.println(r.pos + " r");
+                } else {
+                    ans += Math.abs(curHeight - r.max);
+                    
                 }
             }
         }
         return ans;
         
-    }
 
+    }
     public static class Pair {
-        int pos;
-        int max;
+        int pos, max;
         public Pair(int pos, int max) {
             this.pos = pos;
             this.max = max;
         }
     }
-
 }
