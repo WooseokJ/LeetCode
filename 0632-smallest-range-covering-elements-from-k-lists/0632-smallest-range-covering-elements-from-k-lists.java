be1179e3536091882b0dac8bdfbbc0435a371a08
Wooseok <wooseokbird@gmail.com>
2024-08-15 20:44:04 +0900

    Time: 29 ms (81.88%), Space: 48.7 MB (55.88%) - LeetHub

--- 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.java
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.java
@@ -1,73 +1,83 @@
 import java.util.*;
+
 class Solution {
     public int[] smallestRange(List<List<Integer>> nums) {
         int[] ans = {0,0};
         Queue<Info> pq = new PriorityQueue<>();
-
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
 
-        for(int i =0; i < nums.size(); i++) {
-            // 각 배열의 첫번쨰 원소 꺼내.
-            List<Integer> numList = nums.get(i);
-            int firstVal = numList.get(0);
-            // 큐에 오름차순으로 정렬됨.
-            pq.offer(new Info(firstVal, i, 0));
-            min = Math.min(min, firstVal);
-            max = Math.max(max, firstVal);            
+        for(int i = 0; i < nums.size(); i++) {
+            // 맨 첫항 꺼내 
+            int first = nums.get(i).get(0);
+            pq.offer(new Info(first, i, 0)); // 첫항, 행, 0번쨰 열
+            min = Math.min(min, first);
+            max = Math.max(max, first);
         }
-        // 최소 , 최대값들 ans에 넣기.
+
         ans[0] = min;
         ans[1] = max;
-        int curMinLen = ans[1] - ans[0];
-
-
-        // queue 순환.
+        int curLen = max - min;
         while(!pq.isEmpty()) {
-            // 최소값 뺴줘.
             Info cur = pq.poll();
-            
-            int newLen = max - cur.first;
-            System.out.println(cur.first + " " + newLen + " " + curMinLen);
-            if(newLen < curMinLen) {
-                curMinLen = newLen;
-                System.out.println(curMinLen + " == ");
-                min = cur.first;
-                ans[0] = cur.first;
+            int curFirstVal = cur.first;
+            int curRow = cur.row;
+            int curCol = cur.col;
+
+            int newLen = max - curFirstVal;
+            // 새로운 길이가 더 짧으면? -> 갱신
+            if(newLen < curLen) {
+                curLen = newLen;
+                min = curFirstVal;  //최소값 갱신.
+                ans[0] = min;
                 ans[1] = max;
-            }
-            // col+1 < 현재 배열의 길이 (즉, col이 끝에 도달하는지 아닌지 판단.)
-            if(cur.col + 1 < nums.get(cur.row).size()) {
-                // 위에서 기존 최소값 뺏으니 새로운 최소값 만들어서 넣어줌.
-                int newFirst = nums.get(cur.row).get(cur.col + 1);
-                pq.offer(new Info(newFirst, cur.row, cur.col + 1));
-                max = Math.max(max, newFirst); // 새로꺼낸값과 기존 최대값 비교.
-            
-            } else { // 만약 해당행의 범위가 끝나면 종료
+            }   
+            // 칼럼길이 
+            int colLen = nums.get(curRow).size();
+            int nextCol = curCol + 1;
+            if(nextCol != colLen) {
+                int newFirst = nums.get(curRow).get(nextCol);
+                pq.offer(new Info(newFirst, curRow, nextCol));
+                max = Math.max(max, newFirst); // 최대값 갱신.
+            } else {
                 break;
             }
 
+
+            
         }
-        
         return ans;
+        
 
     }
-
-
-    public static class Info implements Comparable<Info>{
+    public static class Info implements Comparable<Info> {
         int first, row, col;
-        public Info(int first, int row, int col) {
-            this.first = first;
-            this.row = row;
-            this.col = col;
+        public Info(int f, int r, int c) {
+            first = f;
+            row = r;
+            col = c;
         }
         @Override
         public int compareTo(Info o) {
-            return this.first - o.first; // 최소힙. [0,4,5]
-        } 
-
+            return this.first - o.first;
+        }
     }
-
-
 }
 
+// [4 10 15 24 26], 
+// [0,9,12 20], 
+// [5,18,22,30]
+
+// 4 0 5 -> 5 - 0  5   0 제거 
+// 5 4 9 -> 9 - 4  5   5제거
+// 18 4 9 -> 18 - 4    4제거 
+// 18 10 9 -> 18 - 9   9제거 
+// 18 10 12 -> 18 - 10 10제거
+// 18 15 12 -> 18 - 12 12제거
+// 18 15 20 -> 20 - 15 15제거
+// 18 24 20 -> 24 - 18 18제거
+// 22 24 20 -> 24 - 20 20 제거 
+
+// [1 2 3]
+// [1,2,3]
+// [1,2,3]
