08dc03b0d5517e43e0c8a454f2f6307d4fbbea10
Wooseok <wooseokbird@gmail.com>
2024-09-07 13:46:38 +0900

    Time: 23 ms (38.02%), Space: 44.8 MB (39.01%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,37 +1,40 @@
+import java.util.*;
 class Solution {
     public int coinChange(int[] coins, int amount) {
         if(amount == 0) return 0;
         boolean[] visited = new boolean[amount+1];
         return bfs(amount, visited, coins);
-
-
     }
-    public static int bfs(int amount, boolean[] visited, int[] coins) {
+
+    public static int bfs(int amount, boolean[] visited, int[] coins) { 
         Deque<Pair> q = new ArrayDeque<>();
         q.offer(new Pair(amount, 1));
 
         while(!q.isEmpty()) {
             Pair cur = q.poll();
-            for(int i =0; i < coins.length; i++) {
-                int nextAmount = cur.amount - coins[i];
-                if(nextAmount == 0) return cur.ans;
+            
+            for(int coin: coins) {
+                int nextAmount = cur.amount - coin;    
+
+                if(nextAmount == 0) {
+                    return cur.ans;
+                }
                 if(nextAmount > 0 && !visited[nextAmount]) {
+                    
                     visited[nextAmount] = true;
                     q.offer(new Pair(nextAmount, cur.ans + 1));
-                    
-                    
                 }
             }
-            
         }
         return -1;
 
+
     }
     public static class Pair {
-        int amount, ans;
+        int ans , amount ;
         public Pair(int amount, int ans){
-            this.amount = amount;
             this.ans = ans;
+            this.amount = amount;
         }
     }
 }
