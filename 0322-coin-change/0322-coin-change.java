3f2b61e82b98bf6813f24c19288ee32f88c7ed43
Wooseok <wooseokbird@gmail.com>
2024-09-07 13:20:49 +0900

    Time: 22 ms (39.26%), Space: 44.6 MB (43.37%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,34 +1,37 @@
-import java.util.*;
 class Solution {
     public int coinChange(int[] coins, int amount) {
         if(amount == 0) return 0;
         boolean[] visited = new boolean[amount+1];
-
         return bfs(amount, visited, coins);
+
+
     }
     public static int bfs(int amount, boolean[] visited, int[] coins) {
         Deque<Pair> q = new ArrayDeque<>();
         q.offer(new Pair(amount, 1));
 
         while(!q.isEmpty()) {
-            Pair data = q.poll();
-            for(int i = 0; i < coins.length;i++){
-                int next = data.amount - coins[i];
-                if(next == 0) return data.depth;
-                if(next > 0)
-                    if(!visited[next]) {
-                        q.offer(new Pair(next, data.depth+1));
-                        visited[next] = true;
-                    }
+            Pair cur = q.poll();
+            for(int i =0; i < coins.length; i++) {
+                int nextAmount = cur.amount - coins[i];
+                if(nextAmount == 0) return cur.ans;
+                if(nextAmount > 0 && !visited[nextAmount]) {
+                    visited[nextAmount] = true;
+                    q.offer(new Pair(nextAmount, cur.ans + 1));
+                    
+                    
+                }
             }
+            
         }
         return -1;
+
     }
-    public static class Pair{
-        int amount, depth;
-        public Pair(int amount, int depth) {
+    public static class Pair {
+        int amount, ans;
+        public Pair(int amount, int ans){
             this.amount = amount;
-            this.depth = depth;
+            this.ans = ans;
         }
     }
 }
