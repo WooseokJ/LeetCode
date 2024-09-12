0f08ab6895fb997d49a7ebc33225efd93402b0b3
Wooseok <wooseokbird@gmail.com>
2024-09-12 18:16:46 +0900

    Time: 26 ms (25.3%), Space: 50.7 MB (71.88%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -1,21 +1,24 @@
 import java.util.*;
 
 class Solution {
-    // LCS 를 재귀 이용 방식: O(mn)
-    // LCS 를 DP 이용한방식: O(m + n) 
-    public int longestCommonSubsequence(String str1, String str2) { 
-        int m = str1.length();
-        int n = str2.length();
-        int[][] memo = new int[m+1][n+1]; 
-        for(int r = 1; r <= m; r++) { 
-            for(int c = 1; c <= n; c++) {
-                if(str1.charAt(r-1 )== str2.charAt(c-1)) { // 글자 같으면
-                    memo[r][c] =  memo[r-1][c-1] + 1;
+    public int longestCommonSubsequence(String text1, String text2) {
+        int n1 = text1.length();
+        int n2 = text2.length();
+        int[][] memo = new int[n1+1][n2+1];
+        for(int r= 1; r <= n1; r++) {
+            for(int c =1; c<=n2; c++) {
+                Character word = text1.charAt(r-1);
+                Character word2 = text2.charAt(c-1);
+                if(word == word2) {
+                    memo[r][c] = memo[r-1][c-1] + 1;
                 } else {
-                    memo[r][c] =  Math.max(memo[r-1][c], memo[r][c-1]);
+                    memo[r][c] = Math.max(memo[r-1][c] , memo[r][c-1]);
                 }
             }
         }
-        return memo[m][n];
+
+                
+        return memo[n1][n2];
+
     }
 }
