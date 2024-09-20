2df50e8a39a1d524c15d9a47328b4824f95864b6
Wooseok <wooseokbird@gmail.com>
2024-09-20 10:11:59 +0900

    Time: 9 ms (11.52%), Space: 45.3 MB (95.5%) - LeetHub

--- 0792-binary-search/0792-binary-search.java
+++ 0792-binary-search/0792-binary-search.java
@@ -1,28 +1,32 @@
+import java.util.*;
 class Solution {
-    public int search(int[] nums, int target) {
-        // step1. 오름차순 정렬
-        int[] arr = Arrays.copyOf(nums, nums.length);
-        Arrays.sort(arr); // 오름차순
+    public int search(int[] n, int target) {
+        // 정렬
+        List<Integer> temp = new ArrayList<>();
+        for(int i =0; i < n.length; i++)
+            temp.add(n[i]);
+        Collections.sort(temp);
+        int[] nums = new int[temp.size()];
+        for(int i =0; i< nums.length; i++) {
+            nums[i] = temp.get(i);
+        } 
 
-        // step2. left,right 투포인터
         int l = 0;
-        int r = arr.length - 1;
-
-        // step3. target, mid 비교
-        return backtrack(arr, target, l, r);
-    }
-    public static int backtrack(int[] arr, int target, int l , int r){
-        if(l > r) { // >=로 하면안되는건 원소 1개일떄 무조건 -1이 됨. 
-            return -1;
-        }
-        int mid = (l+r) / 2;
-        if(arr[mid] < target) {
-            return backtrack(arr, target, mid+1, r);
-        } else if(arr[mid] > target) {
-            return backtrack(arr, target, l,mid-1);
-        } else {
-            return mid;
+        int r = nums.length -1;
+        while(l <= r) {
+            int mid = (l+r) / 2;
+            if(nums[mid] > target) {
+                r = mid -1;
+            } else if(nums[mid] < target) {
+                l = mid + 1;
+            } else {
+                return mid;
+            }
         }
         
+
+        return -1;
+
+
     }
 }
