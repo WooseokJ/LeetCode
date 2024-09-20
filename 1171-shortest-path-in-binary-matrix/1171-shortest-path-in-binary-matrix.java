b5c1a94ae2f4e48039f38d3a04cbd65ccd2802ff
Wooseok <wooseokbird@gmail.com>
2024-09-20 22:36:21 +0900

    Time: 16 ms (64.18%), Space: 45.4 MB (54.33%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.java
@@ -1,52 +1,57 @@
+import java.util.*;
 class Solution {
-    static int ans = 0;
     public int shortestPathBinaryMatrix(int[][] grid) {
+        int ans = 0;
+
         int rowLen = grid.length;
         int colLen = grid[0].length;
         boolean[][] visited = new boolean[rowLen][colLen];
+        visited[0][0] = true;
         if(grid[0][0] == 1 || grid[rowLen-1][colLen-1] == 1) return -1;
-        bfs(0,0,visited, grid);
         
-        return ans;
-    }
-    public static void bfs(int r, int c, boolean[][] visited, int[][] grid) {
-        int rowLen = grid.length;
-        int colLen = grid[0].length;
-        int[] dr = {1,-1,0,0,1,1,-1,-1};
-        int[] dc = {0,0,1,-1,1,-1,1,-1};
+        Deque<Pair> q = new ArrayDeque<>();
+        q.offer(new Pair(0,0,1));
+        
+        int[] dr = {0,0,1,-1, 1, 1, -1,-1};
+        int[] dc = {1,-1,0,0, 1,-1,1,-1};
 
-        visited[r][c] = true;
-        Deque<int[]> q = new ArrayDeque<>();
-        q.offer(new int[]{r,c,1});
+
         while(!q.isEmpty()) {
-            int[] cur = q.poll();
-            int curRow = cur[0];
-            int curCol = cur[1];
-            int curStep = cur[2];
-
+            Pair cur= q.poll();
+            int curRow = cur.row;
+            int curCol = cur.col;
+            int curDepth = cur.depth;
             if((curRow == rowLen-1) && (curCol == colLen-1)) {
-                ans = curStep;
-                return ;
+                ans = curDepth;
+                return ans;
             }
 
-            for(int i = 0 ;i < 8; i++) {
+            for(int i = 0; i < 8; i++) {
                 int nextRow = curRow + dr[i];
                 int nextCol = curCol + dc[i];
-
-              
                 if(isValid(nextRow, nextCol, rowLen, colLen)) {
                     if(!visited[nextRow][nextCol] && grid[nextRow][nextCol] == 0) {
+                        q.offer(new Pair(nextRow, nextCol, curDepth+1));
                         visited[nextRow][nextCol] = true;
-                        q.offer(new int[]{nextRow, nextCol , curStep+1});
                     }
                 }
             }
-            ans = -1;
+            
         }
-        
+
+        return -1;
+
+    }
+    public static boolean isValid(int r, int c, int rowLen, int colLen) {
+        return (( 0<= r && r < rowLen) && (0 <= c && c < colLen));
     }
 
-    public static boolean isValid(int r, int c, int rowLen, int colLen) {
-        return ((0 <= r && r < rowLen) && (0 <= c && c < colLen));
+    public static class Pair {
+        int row, col, depth;
+        public Pair(int row, int col, int depth) {
+            this.row = row;
+            this.col = col;
+            this.depth = depth;
+        }
     }
 }
