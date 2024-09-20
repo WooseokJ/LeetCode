ba101639addd4c1162856a443539cd0fb32ab81c
Wooseok <wooseokbird@gmail.com>
2024-09-20 10:15:42 +0900

    Time: 2 ms (11.52%), Space: 45.6 MB (63.66%) - LeetHub

--- 0792-binary-search/0792-binary-search.java
+++ 0792-binary-search/0792-binary-search.java
@@ -1,25 +1,17 @@
-import java.util.*;
 class Solution {
     public int search(int[] nums, int target) {
-        // 정렬 
         Arrays.sort(nums);
-
         int l = 0;
-        int r = nums.length -1;
+        int r = nums.length-1;
+
         while(l <= r) {
             int mid = (l+r) / 2;
             if(nums[mid] > target) {
-                r = mid -1;
+                r = mid-1;
             } else if(nums[mid] < target) {
-                l = mid + 1;
-            } else {
-                return mid;
-            }
+                l = mid +1;
+            } else {return mid;}
         }
-        
-
         return -1;
-
-
     }
 }
