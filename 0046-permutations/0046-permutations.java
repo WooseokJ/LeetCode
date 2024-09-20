bc8e0da8aae002f2ce61078b4f930571c4fb2ed7
Wooseok <wooseokbird@gmail.com>
2024-09-20 10:23:01 +0900

    Time: 2 ms (44%), Space: 44.7 MB (12.32%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,24 +1,21 @@
+import java.util.*;
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
         backtrack(new ArrayList<>(), nums, ans);
         return ans;
     }
-    public static void backtrack(List<Integer> curr, int[] nums, List<List<Integer>> ans){ 
-        int n = nums.length;
-        // base case
-        if(curr.size() == n) {
-            ans.add(new ArrayList<>(curr)); // curr에는 현재 nums와 동일한 길이의 배열이 만들어져있다.
-            return;
+    public static void backtrack(List<Integer> curr, int[] nums, List<List<Integer>> ans) {
+        if(curr.size() == nums.length) {
+            ans.add(new ArrayList<>(curr));
+            return ;
         }
-        for(int num: nums) { // nums 원소만큼 순회
-            if(!curr.contains(num)) { // curr에 포함되지않은숫자면 
-                curr.add(num); 
+        for(int i= 0; i < nums.length; i++) {
+            if(!curr.contains(nums[i]))  {
+                curr.add(nums[i]);
                 backtrack(curr, nums, ans);
                 curr.remove(curr.size()-1);
             }
         }
-
-        
     }
 }
