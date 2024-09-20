25230c54a46904e47f53f76da32b572b3c7e8f64
Wooseok <wooseokbird@gmail.com>
2024-09-20 22:09:52 +0900

    Time: 1 ms (98.6%), Space: 41.4 MB (69.88%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -3,27 +3,33 @@ import java.util.*;
 class Solution {
     public boolean isValid(String str) {
 		    // step1. stack선언.
-        Stack<Character> stack = new Stack<>(); 
+        Deque<Character> stack = new ArrayDeque<>(); 
 
         // step2. 문자열길이만큼 순회
         for(int i =0; i < str.length(); i++) { 
             char keyword = str.charAt(i);
             // step3. 해당 키워드가 (, {, [ 중 하나 이면 닫는괄호 ), }, ] 맞게 stack에 저장.
             if(keyword == '(') {
-                stack.add(')');
+                stack.offerLast(')');
             } else if (keyword == '{') {
-                stack.add('}');
+                stack.offerLast('}');
             } else if(keyword == '[') {
-                stack.add(']');
+                stack.offerLast(']');
             } else { // ] ) } 셋중 하나 
-		            // 스택이 비어있지 x + stack의 맨위 원소가 keyword와 같으면
-                if( (!stack.isEmpty()) && (stack.peek() == keyword) ){  // peek()는 top을 반환만.
-                    stack.pop(); // 빼자. pop()는 top을 뺴냄.
-                } else { // 스택 top이외라면 유효하지않음.
-                    return false; 
+		        if(!stack.isEmpty()) {
+                    if(stack.peekLast() == keyword) {
+                        stack.pollLast();
+                    } else {
+                        return false;
+                    }
+                } else {
+                    return false;
                 }
+
             }
         }
+
         return stack.isEmpty();
     }
 }
+
