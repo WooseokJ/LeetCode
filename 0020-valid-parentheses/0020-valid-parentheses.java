631e405f84f11a3129e34fdce3f93bbfd9aff436
Wooseok <wooseokbird@gmail.com>
2024-09-20 21:44:34 +0900

    Time: 1 ms (98.6%), Space: 41.7 MB (24.51%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -2,11 +2,13 @@ import java.util.*;
 
 class Solution {
     public boolean isValid(String str) {
-        Stack<Character> stack = new Stack<>();
-        int n = str.length();
-        
-        for(int i =0; i < n; i++) {
+		    // step1. stack선언.
+        Stack<Character> stack = new Stack<>(); 
+
+        // step2. 문자열길이만큼 순회
+        for(int i =0; i < str.length(); i++) { 
             char keyword = str.charAt(i);
+            // step3. 해당 키워드가 (, {, [ 중 하나 이면 닫는괄호 ), }, ] 맞게 stack에 저장.
             if(keyword == '(') {
                 stack.add(')');
             } else if (keyword == '{') {
@@ -14,8 +16,9 @@ class Solution {
             } else if(keyword == '[') {
                 stack.add(']');
             } else { // ] ) } 셋중 하나 
-                if( (!stack.isEmpty()) && (stack.peek() == keyword) ){ // 비어있지 않고 스택 top의 원소가 keyword와 같으면
-                    stack.pop(); // 빼자.
+		            // 스택이 비어있지 x + stack의 맨위 원소가 keyword와 같으면
+                if( (!stack.isEmpty()) && (stack.peek() == keyword) ){  // peek()는 top을 반환만.
+                    stack.pop(); // 빼자. pop()는 top을 뺴냄.
                 } else { // 스택 top이외라면 유효하지않음.
                     return false; 
                 }
