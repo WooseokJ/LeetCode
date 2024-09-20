bfe56ab13c6c2746e6776f97013d2399eae2cb98
Wooseok <wooseokbird@gmail.com>
2024-09-20 14:01:34 +0900

    Time: 5 ms (31.63%), Space: 51.5 MB (23.4%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,65 +1,43 @@
-import java.util.*;
-
 class Solution {
-    public static int[] dr = {0,0,1,-1};
-    public static int[] dc = {1,-1,0,0};
-
     public int numIslands(char[][] grid) {
-        int ans =0;
+        int ans = 0;
         int rowLen = grid.length;
         int colLen = grid[0].length;
         boolean[][] visited = new boolean[rowLen][colLen];
-        for(int i =0; i < rowLen; i++) {
-            for(int j =0; j < colLen;j++) {
-                if((grid[i][j] == '1' )  && (!visited[i][j])) {
-                    bfs(i,j,visited, grid);
-                    ans++;
-                    
+        for(int r =0; r < rowLen; r++) {
+            for(int c =0; c< colLen; c++) {
+                if((grid[r][c] == '1')) {
+                    if(!visited[r][c]) {
+                        bfs(r,c,visited, grid);
+                        ans++;
+                    }
                 }
-                
             }
         }
         return ans;
-        
-
     }
-    public static void print(Object o) {
-        System.out.println(o);
-    }
-   
     public static void bfs(int r, int c, boolean[][] visited, char[][] grid) {
-        int rowLen = grid.length;
-        int colLen = grid[0].length;
-        Deque<Point> q = new ArrayDeque<>();
-        q.offer(new Point(r,c));
-        visited[r][c] = true;
-        while(!q.isEmpty()) {
-            Point cur = q.poll();
-            int curRow = cur.r;
-            int curCol = cur.c;
-            for(int i =0; i < 4; i++) {
-                int nr = dr[i] + curRow;
-                int nc = dc[i] + curCol;
-                if( (0 <= nr && nr < rowLen) && (0 <= nc && nc < colLen) ) {
-                    if(!visited[nr][nc] && grid[nr][nc] == '1') {
-                        q.offer(new Point(nr, nc));
-                        visited[nr][nc] = true;
-                    }
-                }
+        int[] dr = {1,-1,0,0};
+        int[] dc = {0,0,1,-1};
 
+        visited[r][c] = true;
+        
+        for(int i =0; i < 4; i++) {
+            int nextRow = r + dr[i];
+            int nextCol = c + dc[i];
+            if(isValid(nextRow, nextCol, grid)) {
+                if(!visited[nextRow][nextCol]) {
+                    if(grid[nextRow][nextCol] == '1') {
+                        bfs(nextRow, nextCol, visited, grid);
+                    }
+                    
+                }
             }
         }
-
-
     }
-
-
-    public static class Point {
-        int r, c;
-        public Point(int r, int c) {
-            this.r = r;
-            this.c = c;
-        }
-
+    public static boolean isValid(int r, int c, char[][] grid) {
+        int rowLen = grid.length;
+        int colLen = grid[0].length;
+        return ((0 <= r && r < rowLen ) && (0 <= c && c < colLen));
     }
 }
