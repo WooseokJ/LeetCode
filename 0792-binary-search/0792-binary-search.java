338232fb4dbb9d262dfb81b198c14429f75f3f8c
Wooseok <wooseokbird@gmail.com>
2024-09-20 10:13:25 +0900

    Time: 8 ms (11.52%), Space: 45.3 MB (89.64%) - LeetHub

--- 0792-binary-search/0792-binary-search.java
+++ 0792-binary-search/0792-binary-search.java
@@ -1,21 +1,21 @@
 import java.util.*;
 class Solution {
-    public int search(int[] nums, int target) {
+    public int search(int[] n, int target) {
         // 정렬
-        // List<Integer> temp = new ArrayList<>();
-        // for(int i =0; i < n.length; i++)
-        //     temp.add(n[i]);
-        // Collections.sort(temp);
-        // int[] nums = new int[temp.size()];
-        // for(int i =0; i< nums.length; i++) {
-        //     nums[i] = temp.get(i);
-        // } 
+        List<Integer> temp = new ArrayList<>();
+        for(int i =0; i < n.length; i++)
+            temp.add(n[i]);
+        Collections.sort(temp);
+        int[] nums = new int[temp.size()];
+        for(int i =0; i< nums.length; i++) {
+            nums[i] = temp.get(i);
+        } 
             //step 1 오름차순 정렬
-        int[] arr = Arrays.copyOf(nums, nums.length); // nums가 정렬안되어있다고 가정하므로.
-        Arrays.sort(arr); // 오름차순 
+        // int[] arr = Arrays.copyOf(nums, nums.length); // nums가 정렬안되어있다고 가정하므로.
+        // Arrays.sort(arr); // 오름차순 
 
         int l = 0;
-        int r = arr.length -1;
+        int r = nums.length -1;
         while(l <= r) {
             int mid = (l+r) / 2;
             if(nums[mid] > target) {
